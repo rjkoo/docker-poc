34e6fb70a9db8348d1a92cb13edce5d283f6985a
James Bryant <j.alexanderbryant@gmail.com>
2019-11-05 11:54:08 -0500

    test

--- server/app/extensions.py
+++ server/app/extensions.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_seasurf import SeaSurf
 from flask_login import LoginManager
-
 db = SQLAlchemy()
 mail = Mail()
 csrf = SeaSurf()
